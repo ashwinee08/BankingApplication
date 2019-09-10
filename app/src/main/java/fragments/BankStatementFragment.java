4838bd8f538c1ae8a091e6b28f16b42ade5426c9
Ashwinee Kumar Singh <43812416+ashwinee08@users.noreply.github.com>
2019-09-10 15:49:43 +0530

    Update BankStatementFragment.java
    final BankStatementFragment

--- app/src/main/java/fragments/BankStatementFragment.java
+++ app/src/main/java/fragments/BankStatementFragment.java
@@ -1,32 +1,68 @@
 package fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
+
+import adapters.RecyclerViewAdapter;
+import models.TransactionInfo;
 
 import com.example.joker.bankingapplication.R;
 
-import java.util.List;
+import static adapters.ViewPagerAdapter.getTransactionInfoList;
+import static android.view.View.GONE;
 
-import adapters.ViewPagerAdapter;
-import models.TransactionInfo;
+
 
 public class BankStatementFragment extends Fragment {
 
-    private final List<TransactionInfo> transactionInfoList= ViewPagerAdapter.getTransactionInfoList();
+
+    private Context context;
+    private TextView defaultTextView;
+    private RecyclerView debitsRecyclerView;
+    private List<TransactionInfo> transactionInfoList;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context=context;
+        transactionInfoList = getTransactionInfoList();
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Collections.sort(transactionInfoList, TransactionInfo.compareDate);
     }
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bank_statement,container,false);
+        View view=inflater.inflate(R.layout.fragment_credits,container,false);
+        getHold(view);
+        if(transactionInfoList!=null){
+            defaultTextView.setVisibility(GONE);
+            debitsRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+            debitsRecyclerView.setAdapter(new RecyclerViewAdapter(context,transactionInfoList));
+            debitsRecyclerView.setVisibility(View.VISIBLE);
+        }
+        return view;
+    }
+
+    private void getHold(View view){
+        defaultTextView = view.findViewById(R.id.default_msg_credits);
+        debitsRecyclerView = view.findViewById(R.id.debits_recycler_view_credits);
     }
 }
