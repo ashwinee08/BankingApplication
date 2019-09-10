2d22202068536301127ed3261cd142da0d371452
Ashwinee Kumar Singh <43812416+ashwinee08@users.noreply.github.com>
2019-09-10 15:41:20 +0530

    Update RecyclerViewAdapter.java
    final RecyclerView adapter

--- app/src/main/java/adapters/RecyclerViewAdapter.java
+++ app/src/main/java/adapters/RecyclerViewAdapter.java
@@ -1,93 +1,77 @@
 package adapters;
-
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-
-import java.util.ArrayList;
-import java.util.Collections;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import java.util.List;
-
-import fragments.BankStatementFragment;
-import fragments.CreditsFragment;
-import fragments.DebitsFragment;
 import models.TransactionInfo;
 
-public class ViewPagerAdapter extends FragmentStatePagerAdapter {
-    
-    private static ViewPagerDataProvider viewPagerDataProvider=new ViewPagerDataProvider();
-    private static List<TransactionInfo> transactionInfoList;
-    
-    public ViewPagerAdapter(FragmentManager fm,final List<TransactionInfo> transactionInfoListLocal) {
-        super(fm);
-        transactionInfoList=transactionInfoListLocal;
+import com.example.bankapplication.R;
+
+
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewViewHolder>{
+
+    private Context context;
+    private List<TransactionInfo> transactionInfoList;
+    public RecyclerViewAdapter(Context context, List<TransactionInfo> transactionInfoList) {
+        this.context=context;
+        this.transactionInfoList=transactionInfoList;
     }
-        
-    public static ViewPagerDataProvider getInstance(){
-        return viewPagerDataProvider;
-    }   
-    
-    private static class ViewPagerDataProvider{
-        
-        private ViewPagerDataProvider(){
-        }
-        
-        public List<TransactionInfo> getTransactionInfoListComplete() {
-            final List<TransactionInfo> tempList = transactionInfoList;
-            Collections.sort(tempList, TransactionInfo.compareDate);
-            return tempList;
+
+    @NonNull
+    @Override
+    public RecyclerViewViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_view_element,viewGroup,false);
+        return new RecyclerViewViewHolder(view);
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewViewHolder recyclerViewViewHolder, int i) {
+        TransactionInfo transactionInfo=transactionInfoList.get(i);
+        recyclerViewViewHolder.setDateTextView(transactionInfo.getDate());
+        recyclerViewViewHolder.setAmountTextView(transactionInfo.getAmountDeductedOrCredited());
+        final String TYPE_DEBITED = "debited" ;
+        final String TYPE_CREDITED = "credited" ;
+        if(transactionInfo.getType()== TransactionInfo.BankingType.debited){
+            recyclerViewViewHolder.setTypeTextView(TYPE_DEBITED);
+        }else{
+            recyclerViewViewHolder.setTypeTextView(TYPE_CREDITED);
         }
 
-        public List<TransactionInfo> getTransactionInfoListForSpecificTransaction(TransactionInfo.BankingType transactionType) {
-            final List<TransactionInfo> tempList = new ArrayList<>();
-            for (TransactionInfo tempTransactionInfo : transactionInfoList) {
-                if (tempTransactionInfo.getType() == transactionType) {
-                    tempList.add(tempTransactionInfo);
-                }
-            }
-            return tempList;
-        }
-    }
-    
-     
-    @Override
-    public Fragment getItem(int i) {
-        Fragment toBeReturned;
-        switch(i+1){
-            case 2:
-                toBeReturned=new CreditsFragment();
-                break;
-            case 3:
-                toBeReturned=new BankStatementFragment();
-                break;
-            case 1:
-            default:
-                toBeReturned=new DebitsFragment();
-        }
-        return toBeReturned;
     }
 
     @Override
-    public int getCount() {
-        return 3;
+    public int getItemCount() {
+        return transactionInfoList.size();
     }
 
-    @Nullable
-    @Override
-    public CharSequence getPageTitle(int position) {
-        String tabTitle;
-        switch(position+1){
-            case 2:
-                tabTitle="Credits".toUpperCase();
-                break;
-            case 3:
-                tabTitle="Statement".toUpperCase();
-                break;
-            case 1:
-            default:
-                tabTitle="Debits".toUpperCase();
+    public class RecyclerViewViewHolder extends RecyclerView.ViewHolder{
+
+        private  TextView dateTextView, amountTextView,typeTextView;
+        RecyclerViewViewHolder(@NonNull View itemView) {
+            super(itemView);
+            getHold(itemView);
         }
-        return tabTitle;
+        private void getHold(View view){
+            dateTextView=view.findViewById(R.id.transaction_date);
+            amountTextView=view.findViewById(R.id.transaction__amount);
+            typeTextView=view.findViewById(R.id.transaction_type);
+        }
+
+        public void setDateTextView(String data){
+            dateTextView.setText(data);
+        }
+        public void setAmountTextView(String data){
+            amountTextView.setText(data);
+        }
+        public void setTypeTextView(String data){
+            typeTextView.setText(data);
+        }
     }
 }
