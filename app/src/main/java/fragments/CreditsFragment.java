5efa6ad216e33e02947ab4cb6c2c5629049b423c
Ashwinee Kumar Singh <43812416+ashwinee08@users.noreply.github.com>
2019-09-10 15:48:34 +0530

    Update CreditsFragment.java
    final CreditsFragment

--- app/src/main/java/fragments/CreditsFragment.java
+++ app/src/main/java/fragments/CreditsFragment.java
@@ -1,32 +1,78 @@
 package fragments;
 
+
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.joker.bankingapplication.R;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import adapters.ViewPagerAdapter;
+import adapters.RecyclerViewAdapter;
 import models.TransactionInfo;
+
+import static adapters.ViewPagerAdapter.getTransactionInfoList;
+import static android.view.View.GONE;
 
 public class CreditsFragment extends Fragment {
 
-    private final List<TransactionInfo> transactionInfoList = ViewPagerAdapter.getTransactionInfoList();
+    private Context context;
+    private TextView defaultTextView;
+    private RecyclerView debitsRecyclerView;
+    private  List<TransactionInfo> transactionInfoList;
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context=context;
+        transactionInfoList= getTransactionInfoList();
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        transactionInfoList= getTransactionInfoListForSpecificTransaction(transactionInfoList, TransactionInfo.BankingType.credited);
+    }
+
+    private static List<TransactionInfo> getTransactionInfoListForSpecificTransaction(final List<TransactionInfo> transactionInfoList,TransactionInfo.BankingType transactionType) {
+        final List<TransactionInfo> tempList = new ArrayList<>();
+        if(transactionInfoList!=null){
+            for (TransactionInfo tempTransactionInfo : transactionInfoList) {
+                if (tempTransactionInfo.getType() == transactionType) {
+                    tempList.add(tempTransactionInfo);
+                }
+            }
+        }
+        return tempList;
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_credits, container, false);
+        View view=inflater.inflate(R.layout.fragment_credits,container,false);
+        getHold(view);
+        if(transactionInfoList!=null){
+            defaultTextView.setVisibility(GONE);
+            debitsRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+            debitsRecyclerView.setAdapter(new RecyclerViewAdapter(context,transactionInfoList));
+            debitsRecyclerView.setVisibility(View.VISIBLE);
+        }
+        return view;
+    }
+
+    private void getHold(View view){
+        defaultTextView = view.findViewById(R.id.default_msg_credits);
+        debitsRecyclerView = view.findViewById(R.id.debits_recycler_view_credits);
     }
 }
+
